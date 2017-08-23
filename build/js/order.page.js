12ef8ccda60c2b9082000e995b091235ae1761c0
penglunjian <875831103@qq.com>
2017-08-24 07:31:13 +0800

    update this project

---
+++ build/js/order.page.js
@@ -0,0 +1,49 @@
+/**
+ * BEGIN 编写首页插件
+ * Author:PengLunJian
+ * Date:2017-08-24
+ * @constructor 构造函数
+ */
+function OrderPage() {
+    var arguments = arguments.length != 0 ? arguments[0] : arguments;
+    this.active = arguments['active'] ? arguments['active'] : 'active';
+    this.expItem = arguments['expItem'] ? arguments['expItem'] : '.exp-item';
+    this.init();
+}
+/**
+ * BEGIN 初始化首页插件
+ * Author:PengLunJian
+ * Date:2017-08-24
+ * @returns {OrderPage} 返回当前对象实现连缀调用
+ */
+OrderPage.prototype.init = function () {
+    /**
+     * BEGIN 实例化延时加载插件
+     * Author:PengLunJian
+     * Date:2017-08-24
+     * @type {Lazyload}
+     */
+    var lazy = new Lazyload();
+    this.selectExpressItem();
+}
+/**
+ *
+ * @returns {OrderPage}
+ */
+OrderPage.prototype.selectExpressItem = function () {
+    var _this = this;
+    $(document).on('click', this.expItem, function () {
+        $(_this.expItem).removeClass(_this.active);
+        $(this).addClass(_this.active);
+    });
+    return this;
+}
+/**
+ * BEGIN 实例化首页插件
+ * Author:PengLunJian
+ * Date:2017-08-23
+ * @type {ClubPage}
+ */
+var orderPage = new OrderPage();
+
+
